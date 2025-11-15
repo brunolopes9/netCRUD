d0715436d6dbb5dc13f4e1068777bd581c0bf62a
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-11-15 17:36:41 +0000

    Create Endpoinst API, GET ,GET(id) , POST

---
+++ backend/crudNET/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using crudNET.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudNET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _employeeContext;
+        public EmployeeController(EmployeeContext employeeContext)
+        {
+            _employeeContext = employeeContext;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+        if(_employeeContext.Employees==null)
+            {
+                return NotFound();
+            }
+            return await _employeeContext.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployees(int id)
+        {
+            if (_employeeContext.Employees == null)
+            {
+                return NotFound();
+            }
+            var employee = await _employeeContext.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _employeeContext.Employees.Add(employee);
+            await _employeeContext.SaveChangesAsync();
+
+            return CreatedAtFunction(nameof(GetEmployee), new { id = employee.ID }, employee);
+        }
+
+
+    }
+}
