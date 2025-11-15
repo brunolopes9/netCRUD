641d7e283dcc5ef6d73cdf8486a85bbeba3c25cc
Bruno Lopes <bruno-lopes9@hotmail.com>
2025-11-15 16:07:52 +0000

    Installed packages 8.0 NET, and add Cors and Employee Context creation

--- backend/crudNET/Program.cs
+++ backend/crudNET/Program.cs
@@ -1,4 +1,7 @@
 
+using crudNET.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace crudNET
 {
     public class Program
@@ -9,10 +12,17 @@ namespace crudNET
 
             // Add services to the container.
 
+            builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CRUDCS"));
+
+
+
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors();
 
             var app = builder.Build();
 
@@ -22,6 +32,15 @@ namespace crudNET
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors(builder =>
+            {
+                builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            });
+
 
             app.UseHttpsRedirection();
 
